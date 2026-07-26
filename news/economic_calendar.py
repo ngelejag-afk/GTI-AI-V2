c3f3d088bb0c90845bcb949d6ce1f927fa512747
ngelejag-afk <ngelejag@gmail.com>
2026-07-26 23:40:57 +0300

    Add Economic Calendar
    Add EconomicCalendar class to interface with news filter.

--- news/economic_calendar.py
+++ news/economic_calendar.py
@@ -1,1 +1,27 @@
+"""
+GTI AI
+Economic Calendar
+Version 1.0
+"""
 
+from news.news_filter import NewsFilter
+
+
+class EconomicCalendar:
+    """
+    Economic calendar interface.
+    """
+
+    @staticmethod
+    def trading_allowed() -> bool:
+        """
+        Returns whether trading is currently allowed.
+        """
+        return NewsFilter.trading_allowed()
+
+    @staticmethod
+    def events() -> list[dict]:
+        """
+        Returns high-impact events.
+        """
+        return NewsFilter.high_impact_events()
