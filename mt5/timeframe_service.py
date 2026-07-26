60ae42d7e075ee17aa2a97ad0df2a9f31220357f
ngelejag-afk <ngelejag@gmail.com>
2026-07-26 22:57:17 +0300

    Add Timeframe Service
    Implemented a TimeframeService class to provide MetaTrader 5 timeframe constants and a method to return all supported timeframes.

--- mt5/timeframe_service.py
+++ mt5/timeframe_service.py
@@ -1,1 +1,42 @@
+"""
+GTI AI
+MT5 Timeframe Service
+Version 1.0
+"""
 
+import MetaTrader5 as mt5
+
+
+class TimeframeService:
+    """
+    Provides MetaTrader 5 timeframe constants.
+    """
+
+    M1 = mt5.TIMEFRAME_M1
+    M5 = mt5.TIMEFRAME_M5
+    M15 = mt5.TIMEFRAME_M15
+    M30 = mt5.TIMEFRAME_M30
+
+    H1 = mt5.TIMEFRAME_H1
+    H4 = mt5.TIMEFRAME_H4
+
+    D1 = mt5.TIMEFRAME_D1
+    W1 = mt5.TIMEFRAME_W1
+    MN1 = mt5.TIMEFRAME_MN1
+
+    @staticmethod
+    def all() -> dict:
+        """
+        Returns all supported timeframes.
+        """
+        return {
+            "M1": TimeframeService.M1,
+            "M5": TimeframeService.M5,
+            "M15": TimeframeService.M15,
+            "M30": TimeframeService.M30,
+            "H1": TimeframeService.H1,
+            "H4": TimeframeService.H4,
+            "D1": TimeframeService.D1,
+            "W1": TimeframeService.W1,
+            "MN1": TimeframeService.MN1,
+        }
