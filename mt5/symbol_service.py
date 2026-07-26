41e18c148dd55b9d577cf5c977ea7338fe08263e
ngelejag-afk <ngelejag@gmail.com>
2026-07-26 22:52:25 +0300

    Add Symbol Service
    Implemented SymbolService class to manage symbol information and visibility in MetaTrader 5.

--- mt5/symbol_service.py
+++ mt5/symbol_service.py
@@ -1,1 +1,50 @@
+"""
+GTI AI
+MT5 Symbol Service
+Version 1.0
+"""
 
+import MetaTrader5 as mt5
+
+
+class SymbolService:
+    """
+    Reads symbol information from MetaTrader 5.
+    """
+
+    @staticmethod
+    def get(symbol: str) -> dict | None:
+        """
+        Returns symbol information.
+        """
+        info = mt5.symbol_info(symbol)
+
+        if info is None:
+            return None
+
+        return {
+            "symbol": info.name,
+            "description": info.description,
+            "bid": info.bid,
+            "ask": info.ask,
+            "spread": info.spread,
+            "digits": info.digits,
+            "point": info.point,
+            "trade_mode": info.trade_mode,
+            "visible": info.visible,
+        }
+
+    @staticmethod
+    def ensure_visible(symbol: str) -> bool:
+        """
+        Makes a symbol visible in Market Watch.
+        """
+        info = mt5.symbol_info(symbol)
+
+        if info is None:
+            return False
+
+        if info.visible:
+            return True
+
+        return mt5.symbol_select(symbol, True)
