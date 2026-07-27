e7e2394bbeb29886a18db41e59f374e4a7a1697c
ngelejag-afk <ngelejag@gmail.com>
2026-07-28 02:41:10 +0300

    Add global system configuration
    Added global system settings for trading configuration.

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,29 @@
+"""
+GTI AI
+System Configuration
+Version 1.0
+"""
 
+
+class Settings:
+    """
+    Global system settings.
+    """
+
+    SYMBOL = "XAUUSD"
+
+    TIMEFRAME = "M15"
+
+    RISK_PERCENT = 1.0
+
+    ATR_MULTIPLIER = 1.5
+
+    DEFAULT_BALANCE = 10000.0
+
+    PAPER_TRADING = True
+
+    LIVE_TRADING = False
+
+    MAGIC_NUMBER = 2026001
+
+    JOURNAL_FILE = "trade_history.csv"
