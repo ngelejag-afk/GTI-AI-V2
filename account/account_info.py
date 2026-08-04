dea657d5d569a2d54943dfc4faeb7304f1f7bf22
ngelejag-afk <ngelejag@gmail.com>
2026-08-04 20:54:37 +0300

    Implement AccountInfo class for account details
    Added AccountInfo class to wrap AccountEngine methods for account details.

--- account/account_info.py
+++ account/account_info.py
@@ -1,1 +1,29 @@
+"""
+GTI AI
+Account Info Module
+"""
+
+from __future__ import annotations
+from account.account_engine import AccountEngine
+
+
+class AccountInfo:
+    """
+    Wrapper interface for AccountEngine to provide account details.
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    @property
+    def balance(self) -> float:
+        return AccountEngine.balance()
+
+    @property
+    def equity(self) -> float:
+        return AccountEngine.equity()
+
+    @classmethod
+    def get_summary(cls) -> dict:
+        return AccountEngine.summary()
 
